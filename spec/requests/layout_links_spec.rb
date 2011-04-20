92dcd6b68097fa2edd2e18e76c364f1923f4c425
Andreas Finger <webmaster@mediafinger.com>
2011-04-20 13:59:53 +0200

    gem redcarpet instead of rdiscount

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -28,6 +28,21 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "sign up")
   end
 
+  it "should have working links on the layout" do
+    visit root_path
+    click_link "about"
+    response.should have_selector('title', :content => "about")
+    click_link "standing out"           # logo image links back to root
+    response.should have_selector('title', :content => "home")
+    click_link "contact"
+    response.should have_selector('title', :content => "contact")
+    click_link "home"
+    response.should have_selector('title', :content => "home")
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "sign up")
+    click_link "help"
+    response.should have_selector('title', :content => "help")
+  end
 
 end
 
