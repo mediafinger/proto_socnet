8b224e44a7c86eb8fbe1c0a0bb3e3cb644138919
Andreas Finger <webmaster@mediafinger.com>
2011-05-25 00:35:22 +0200

    Sign up working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
     @title = "sign up"
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome, #{@user.name}!"
+      #sign_in @user
+      redirect_to @user
+    else
+      @title = "Please try to sign up again"
+      @user.password = ''
+      @user.password_confirmation = ''
+      render :new
+    end
   end
 
   def show
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -16,6 +16,54 @@ describe UsersController do
   end
 
 
+  describe "POST 'create'" do
+    describe "failure" do
+      before :each do
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => "" }
+      end
+
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "sign up")
+      end
+
+      it "should render the new page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+       end
+    end
+
+    describe "success" do
+      before :each do
+        @attr = { :name => "New User", :email => "valid@email.org",
+                  :password => "foobar", :password_confirmation => "foobar" }
+      end
+
+      it "should create a new user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to the show user page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it "should display a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome/i
+      end
+    end
+  end
+
+
   describe "GET 'show'" do
     before :each do
       @user = Factory(:user)
